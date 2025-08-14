ab4097ba899546b9d2c570e6494f86866678e6f9
vamshi Amudala <vamshipatel126@gmail.com>
2025-08-14 19:45:20 +0530

    new change

--- search-service/src/index.js
+++ search-service/src/index.js
@@ -5,15 +5,15 @@ const registerWithEureka = require('./eureka-client');
 const app = express();
 const PORT = process.env.PORT || 3002;
 
-// Middleware
+
 app.use(express.json());
 
-// Routes
+
 app.get('/search', (req, res) => {
     res.json({ message: 'Search Service is working!' });
 });
 
-// Start server
+
 app.listen(PORT, () => {
     console.log(`Search Service running on port ${PORT}`);
     registerWithEureka('search-service', PORT);
